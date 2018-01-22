89651d24519e74e4b76df044642d83363428dc1b
eiriksandberg <eirik.sandberg@live.no>
2018-01-22 13:04:05 +0100

    Initial commit

---
+++ preprocess.py
@@ -0,0 +1,49 @@
+from music21 import *
+
+def noteHandler(timestep):
+    m21class = str(timestep.__class__)
+    # If timestep is a chord
+    if (m21class == '<class \'music21.chord.Chord\'>'):
+        l = []
+        for p in timestep.pitches: #iterates through all notes and converts to right pitch
+            l.append(p.ps)
+        t = {'class': 'chord', 'pitches': l, 'duration': timestep.duration} #Adds list of pitches in chord and duration to a dict.
+        return t
+    if (m21class == '<class \'music21.note.Rest\'>'):
+        t = {'class1': 'rest', 'pitches': [], 'duration': timestep.duration}
+
+    if (m21class == '<class \'music21.note.Note\'>'):
+        l = []
+        for p in timestep.pitches: # Find pitch of note
+            l.append(p.ps)
+        t = {'class': 'note', 'pitches': l, 'duration': timestep.duration}
+        print(t)
+
+
+s = converter.parse('MIDI/20th.mid')
+print(s.__class__)
+
+#Stream
+data = s.parts
+print(len(data))
+
+#data.show('text')
+
+
+s2 = instrument.partitionByInstrument(s)
+#s2 = instrument.unbundleInstruments(s)
+
+
+for p in s2.parts:
+    unused = p.makeRests(fillGaps=True, inPlace=True)
+
+#s2.show('text')
+for i in s2:
+    for timestep in i:
+        l = []
+        l.append(noteHandler(timestep))
+    print(l)
+
+# Print out ID from stream
+#for p in partStream:
+#    print(p.id)

955482070caa84b1268219350b68a2ed8446dd61
eiriksandberg <eirik.sandberg@live.no>
2018-04-11 11:00:00 +0200

    moved encoder and decoder to own files

---
+++ neuralnetwork/encoder.py
@@ -0,0 +1,32 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+import torch.nn.functional as F
+import torchvision
+from torchvision import transforms
+import torch.optim as optim
+from torch import nn
+
+'''
+class Encoder(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(Encoder, self).__init__()
+        self.LSTM = torch.nn.LSTM(D_in, H, 1, batch_first=True)
+        self.linear1 = torch.nn.Linear(H, H)
+        self.linear2 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        output, hn = self.LSTM(x, None)
+        x = F.relu(self.linear1(output))
+        return F.relu(self.linear2(x))
+'''
+class Encoder(torch.nn.Module):
+    def __init__(self, D_in, H, D_out):
+        super(Encoder, self).__init__()
+        self.LSTM = torch.nn.LSTM(D_in, H, 1, batch_first=True)
+        self.linear1 = torch.nn.Linear(H, D_out)
+
+    def forward(self, x):
+        output, hn = self.LSTM(x, None)
+        x = F.relu(self.linear1(output))
+        return x
